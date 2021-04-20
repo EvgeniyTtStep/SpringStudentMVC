6decd1d4c31bd72457cd87df57a5fbdb1b56417f
Evgeniy <46program@gmail.com>
2021-04-20 18:39:56 +0300

    Create first mvc project

---
+++ src/main/java/com/itstep/controller/MyFirstController.java
@@ -0,0 +1,37 @@
+package com.itstep.controller;
+
+import com.itstep.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstController {
+
+    @GetMapping("/hello")
+    @ResponseBody//выводит ответ на страницу
+    public String hello() {
+        return "hello from controller";
+    }
+
+    @GetMapping({"/show","/show/{first}", "/show/{first}/{second}"})
+    public String show(@PathVariable(value = "first", required = false)String first,
+                       @PathVariable(value = "second", required = false)String second,
+                       @RequestParam(value = "name", required = false, defaultValue = "Noname")String name,
+                       @RequestParam(value = "surname", required = false, defaultValue = "Noname")String surname,
+                       Model model) {//@RequestParam() - аналог getParametr()
+        model.addAttribute("name", "Controller name " + name);
+        model.addAttribute("surname", "Controller surname " + surname);
+        return "show";
+    }
+
+    @GetMapping("/student")
+    public String showStudent(Model model){
+        Student student = new Student("Ivan", "1234");
+        model.addAttribute("student", student);
+        return "show";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/show.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Evgeniy
+  Date: 20.04.2021
+  Time: 16:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Show controller</h1>
+<h2>${name}</h2>
+<h2>${surname}</h2>
+<h2>${first}</h2>
+<h2>${second}</h2>
+
+Student
+<h3>Name = ${student.name}</h3>
+<h3>Phone = ${student.phone}</h3>
+</body>
+</html>
